16ca630cf75c36b75c48b475651dcf2fc8340cd3
Shameer Mukhtar <92436805+IamShameerMukhtar@users.noreply.github.com>
2024-04-16 09:39:30 +0500

    Add Blogs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 const app = express();
 // Connect to Mongodb
 const dbURI = "mongodb+srv://root:root@blog.xw5ksth.mongodb.net/";
@@ -17,6 +18,21 @@ mongoose
 app.set("view engine", "ejs");
 
 // mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "This is my new first blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.listen(4000);
 // middle for static files
@@ -52,4 +68,3 @@ app.get("/blogs/create", (req, res) => {
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
-// dynamic data ke liay hum view engine ya template engine use krtay hai
