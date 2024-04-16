5e31762baadb96d51ccd7ad5f11a0ec410d10331
Shameer Mukhtar <92436805+IamShameerMukhtar@users.noreply.github.com>
2024-04-16 10:50:08 +0500

    Delete sandbox routes

--- app.js
+++ app.js
@@ -8,51 +8,15 @@ const dbURI = "mongodb+srv://root:root@blog.xw5ksth.mongodb.net/";
 mongoose
   .connect(dbURI)
   .then((result) => {
+    //console.log(result);
     console.log("Connected");
   })
   .catch((err) => {
-    console.log("Sorry some issue", err);
+    console.log(err);
   });
 // view engine
 
 app.set("view engine", "ejs");
-
-// mongoose and mongo sandbox routes
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "2nd blog",
-    snippet: "about my new blog",
-    body: "This is my new first blog",
-  });
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-// view Blogs
-app.get("/all-blog", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-// view single blog
-app.get("/single-blog", (req, res) => {
-  Blog.findById("661dff1d89f75e239bdce9ee")
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
 
 app.listen(4000);
 // middle for static files
@@ -69,13 +33,20 @@ app.use(morgan("dev"));
 
 // Handle Requests
 app.get("/", (req, res) => {
-  const blogs = [
-    { title: "Shameer is here", snippet: "hello Shameer" },
-    { title: "Khurram is here", snippet: "hello Khrram" },
-    { title: "Ali is here", snippet: "hello ali" },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
+// view blogs
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "Home", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
