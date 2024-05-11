8927299bbcab48244bd4efa2c961b82f9433124d
Shameer Mukhtar <92436805+IamShameerMukhtar@users.noreply.github.com>
2024-05-11 21:51:40 +0500

    MVC V2

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,39 +3,13 @@ const blogController = require("../controllers/blogController");
 const router = express.Router();
 
 router.get("/", blogController.blog_index);
-
 // post blogs
-router.post("/", (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then(() => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
+router.post("/", blogController.blog_create_post);
 // create
 router.get("/create", blogController.blog_create_get);
-
 // View single Blog
 router.get("/:id", blogController.blog_details);
-
-// Delete Blogs
-
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then(() => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
+// Del ete Blogs
+router.delete("/:id", blogController.blog_create_delete);
 // about
-
 module.exports = router;
