369c78dd32d431bd00874b4925bc72fb1311f77d
Shameer Mukhtar <92436805+IamShameerMukhtar@users.noreply.github.com>
2024-07-07 04:29:54 +0500

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,11 +5,11 @@ const router = express.Router();
 router.get("/", blogController.blog_index);
 // post blogs
 router.post("/", blogController.blog_create_post);
-// create
+// View all blogs
 router.get("/create", blogController.blog_create_get);
-// View single Blog
+// View single Blog using id
 router.get("/:id", blogController.blog_details);
-// Del ete Blogs
+// Delete Blogs using id
 router.delete("/:id", blogController.blog_create_delete);
 // about
 module.exports = router;
